96b6a32a1f07e9d6a7bc65be7ece98e0263b167d
ez2create <143348447+ez2create@users.noreply.github.com>
2023-11-30 05:34:49 +0000

    day4

--- index.js
+++ index.js
@@ -17,14 +17,16 @@ var htmlTaskContents = ({ id, title, type, url, description }) => `
                 </button>
             </div>
             <div class="card-body task_body">
-                ${url$$`
-                    <img src=${url} alt="Image Cap" width="100%" class="card-img-top">`};
+                ${
+                  url &&
+                  `<img src=${url} alt="Image Cap" width="100%" class="card-img-top md-3 rounded-lg" />`
+                }
                 <h4 class="card-title data-gram_editor='false'">${title}</h4>
                 <p class="description trim-3-lines text-muted data-gram_editor='false'">${description}</p>
                 <div class="tags-text text-white d-flex flex-wrap">
                     <span class="badge bg-primary">${type}</span>
                 </div>
-
+ 
             </div>
             <div class="card-footer task_footer">
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#showTask">
@@ -34,3 +36,39 @@ var htmlTaskContents = ({ id, title, type, url, description }) => `
         </div>
     </div>
     `;
+var htmlModalContents = ({ title, id, description, url }) => {
+  var date = new Date(parseInt(id));
+  return `
+        <div id=${id}>
+        ${
+          url &&
+          `<img src=${url} alt="Image Cap" width="100%" class="card-img-top md-3 rounded-lg" />`
+        }
+        <strong class="text-sm text-muted">Created on ${date.toDateString()}</strong>
+        <h2 class="my-3">${title}</h2>
+        <p class="lead">${description}</p>
+        </div>`;
+};
+
+var updatelocalStorage = () => {
+  localStorage.setItem("task", JSON.stringify({ tasks: state.taskLists() }));
+};
+
+var loadInitialData = () => {
+  var localStorageCopy = JSON.parse(localStorage.tasks());
+  if (localStorageCopy) state.taskLists = localStorageCopy.tasks;
+
+  state.taskLists.map((cardData) => {
+    taskContents.insertAdjacentHTML("beforeend", htmlTaskContents(cardData));
+  });
+};
+
+var handleSubmit = (event) => {
+  const id = `${Date.now()}`;
+  const input = {
+    url: document.getElementById("imageURL").value,
+    title: document.getElementById("taskTitle").value,
+    tag: document.getElementById("taskTag").value,
+    description: document.getElementById("taskDescrip").value,
+  };
+};
